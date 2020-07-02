28156d1698f6887e1fdb9c2baff231e9c6f47ad6
Sebastian Gruia <34604352+sebagruia@users.noreply.github.com>
2020-07-02 22:33:42 +0300

    Fixed the Responsiveness of the web app

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,9 +2,7 @@ import React, { useState } from "react";
 import "./header.css";
 import { connect } from "react-redux";
 import { useHistory } from "react-router-dom";
-// import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
@@ -24,44 +22,42 @@ const Header = (props) => {
     event.preventDefault();
     dispatch(searchMoviesAction(query));
     history.push("/search", { query: query });
+    setQuery("");
   };
 
-  const redirectToHome = ()=>{
+  const redirectToHome = () => {
     history.push("/");
-  }
+  };
 
   return (
     <div className="container">
       <Navbar expand="lg">
         <hr className="header-hr" />
-        
-          <Navbar.Brand href="#home" onClick={redirectToHome} role="button">
-            <img
-              src="/logo dark blue.png"
-              className="d-inline-block align-top"
-              alt="Cinematrix logo"
-            />
-          </Navbar.Brand>
-        
 
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-        {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-        {/* <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav> */}
+        <Navbar.Brand href="#home" onClick={redirectToHome} role="button">
+          <img
+            src="/logo dark blue.png"
+            className="d-inline-block align-top"
+            alt="Cinematrix logo"
+          />
+        </Navbar.Brand>
+
         <Form inline className="ml-auto" onSubmit={handleOnSubmit}>
           <FormControl
+            value={query}
             onChange={handleOnChange}
             type="text"
             placeholder="Search"
             className="mr-sm-2"
           />
-          <Button type="submit" variant="outline-light">
+          <Button
+            className="search-button"
+            type="submit"
+            variant="outline-light"
+          >
             Search
           </Button>
         </Form>
-        {/* </Navbar.Collapse> */}
       </Navbar>
     </div>
   );
