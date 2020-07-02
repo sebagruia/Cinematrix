e46a3897a70db138375ce2cc92d192ad27d90448
Sebastian Gruia <34604352+sebagruia@users.noreply.github.com>
2020-07-02 22:54:24 +0300

    Updates

---
+++ precache-manifest.d99ea98246678df1f07a2d6f59e5042a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2ded4576ffea64a41c5f32319407b8",
+    "url": "/Cinematrix/index.html"
+  },
+  {
+    "revision": "9f31b48bf9094c6cfd32",
+    "url": "/Cinematrix/static/css/main.2638639b.chunk.css"
+  },
+  {
+    "revision": "39ecab01e67b341c2a43",
+    "url": "/Cinematrix/static/js/2.7493d6aa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Cinematrix/static/js/2.7493d6aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f31b48bf9094c6cfd32",
+    "url": "/Cinematrix/static/js/main.c539d453.chunk.js"
+  },
+  {
+    "revision": "fe12bbbdc06a0223ffce",
+    "url": "/Cinematrix/static/js/runtime-main.7deb2310.js"
+  },
+  {
+    "revision": "fbd386c13999f7834206b310c1cff619",
+    "url": "/Cinematrix/static/media/red-chairs.fbd386c1.jpg"
+  },
+  {
+    "revision": "8b2b68f88fa7370f6f42f105f7fd8af9",
+    "url": "/Cinematrix/static/media/theMovieAPI.8b2b68f8.svg"
+  }
+]);
